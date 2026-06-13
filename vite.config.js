9e75837638cc45239b232e48ae48488debe52bd0
Syedfaisa1 <faisalsyed7786@gmail.com>
2026-06-13 17:51:22 +0530

    port number updated

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+  plugins: [react()],   // <-- this was missing
+  server: {
+    port: 9000,
+    strictPort: true
+  }
 })
